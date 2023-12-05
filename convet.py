afc1a95784d3e8a7f20fd24e5522f54c66a129c0
nnn <27625496+nnn112358@users.noreply.github.com>
2023-12-06 07:33:08 +0900

    Update convet.py

--- convet.py
+++ convet.py
@@ -2,6 +2,10 @@
 import os
 import torch
 
+os.system("mkdir model") 
+os.system("cd model") 
+
+
 model = torch.hub.load("PeterL1n/RobustVideoMatting", "mobilenetv3")  # or "resnet50"
 model.cpu()
 model.eval()
@@ -9,6 +13,8 @@ model.eval()
 traced_script_module = torch.jit.trace(model, torch.randn(1, 3, 512, 512), strict=False)
 traced_script_module.save("rvm_ts.pt")
 
-os.system("./pnnx rvm_ts.pt inputshape=[1,3,512,512] device=cpu") 
+os.system("wget https://github.com/pnnx/pnnx/releases/download/20231127/pnnx-20231127-ubuntu.zip") 
+os.system("unzip pnnx-20231127-ubuntu.zip") 
+os.system("./pnnx-20231127-ubuntu/pnnx rvm_ts.pt inputshape=[1,3,512,512] device=cpu") 
 
 
